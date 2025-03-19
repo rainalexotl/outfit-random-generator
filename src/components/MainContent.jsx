4448ffe01fdc2a9cd99caf3120172c5a95db7ef4
rainalexotl <rainalex01@gmail.com>
2025-03-19 03:28:50 -0400

    implemented outfit category selection ui

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,11 @@
 import WardrobeTable from "./WardrobeTable.jsx";
+import OutfitFilter from "./OutfitFilter.jsx";
 
 export const MainContent = () => {
     return (
         <main className="flex flex-row h-full">
             <WardrobeTable />
+            <OutfitFilter />
             <section className="w-1/3 bg-violet-800">
                 <p>Outfit goes here goes here</p>
             </section>
---
+++ src/components/OutfitFilter.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { categoryColors } from "../data";
+
+const categoryOptions = Object.keys(categoryColors);
+
+export const OutfitFilter = () => {
+
+    const [ selectedOptions, setSelectedOptions ] = useState(new Set());
+
+    const handleCategoryChange = (event) => {
+        const { value, checked } = event.target;
+        
+        setSelectedOptions((prevOptions) => {
+            let newOptions = new Set(prevOptions);
+            if ( checked ) {
+                newOptions.add(value);
+            } else {
+                newOptions.delete(value);
+            }
+
+            return newOptions;
+        });
+    }
+
+    return (
+        // flex flex-col items-center min-w-[125px]
+        <section className="flex flex-col items-center justify-center w-1/3">
+            <fieldset>
+                {categoryOptions.map((category) =>
+                    <div key={category} className="flex items-center gap-3 mb-2.5">
+                        <input 
+                            className="size-5 accent-neutral-600 "
+                            type="checkbox" 
+                            id={category} 
+                            name="Category" 
+                            value={category}
+                            onChange={handleCategoryChange} 
+                        />
+                        <label htmlFor={category} className="font-main font-semibold text-neutral-600">{category}</label>
+                    </div>
+                )}
+            </fieldset>
+
+            <button 
+                disabled={selectedOptions.size === 0} 
+                className="w-[110px] mt-5 px-6 py-2 rounded-[10px] font-main font-semibold text-sm bg-enabled-button text-neutral-600 disabled:bg-disabled-buttons disabled:text-neutral-400"
+                onClick={() => console.log("Genearate Clicked", selectedOptions)}
+            >
+                Generate
+            </button>
+        </section>
+    );
+}
+
+export default OutfitFilter;
