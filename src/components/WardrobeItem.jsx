4b7deed668b4e6a674a853aa6b8fd0768b1dcbf8
rainalexotl <rainalex01@gmail.com>
2025-03-19 01:03:58 -0400

    fixed problem caused by trying to dynamically create a class name -> not supported by tailwind

--- src/components/WardrobeItem.jsx
+++ src/components/WardrobeItem.jsx
@@ -1,6 +1,14 @@
+import { categoryColors } from "../data.js";
+
 export default function WardrobeItem({ item }) {
+
+    let itemClasses = "grid grid-cols-3 mb-[15px] rounded-[10px] \
+    px-[15px] py-[12px] font-main font-semibold text-neutral-600 text-sm "
+    const bgColor = categoryColors[item.category];
+    itemClasses += bgColor;
+
     return (
-        <div className={`grid grid-cols-3 mb-[15px] bg-${item.category.toLowerCase()} rounded-[10px] px-[15px] py-[12px] font-main font-semibold text-neutral-600 text-sm`} >
+        <div className={itemClasses} >
             <p className="col-span-2">{item.name}</p>
             <p className="text-right">{item.category}</p>
         </div>
