9605a45502ab2714323fb0e0f0e9e3b986edf153
rainalexotl <rainalex01@gmail.com>
2025-03-18 23:59:31 -0400

    added bottom margin and gap between table update buttons

--- src/components/TableUpdateButtons.jsx
+++ src/components/TableUpdateButtons.jsx
@@ -2,7 +2,7 @@ import Icon from "./Icon.jsx";
 
 export default function TableUpdateButtons() {
     return (
-        <section className="ml-[30px]">
+        <section className="flex flex-row gap-3 ml-[30px] mb-2">
             <button>
                 <Icon iconName={"add"} color="text-neutral-600" />
             </button>
